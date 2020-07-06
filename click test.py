120d204b87a69055d0fd636f660434cc43dd4593
n1ckxd <67688823+n1ckxd@users.noreply.github.com>
2020-07-06 22:29:07 +0300

    Update click test.py

--- click test.py
+++ click test.py
@@ -127,7 +127,7 @@ def click():
         if timegame < 1:
             clicksgame = clicksgame+1
             lblclicks['text'] = ("Клики:", clicksgame)
-lbltitle = Label(window, font = "Times 13", text = "Приветствую вас в моей программе!\nЗдесь вы сможете узнать,сколько кликов вы сможете сделать за 10 секунд.\nПо умолчанию тест идёт на 10 секунд.")
+lbltitle = Label(window, font = "Times 13", text = "Приветствую вас в моей программе!\nЗдесь вы сможете узнать,сколько кликов вы сможете сделать за 10 или 5 секунд.\nПо умолчанию тест идёт на 10 секунд.")
 lblspace = Label(window, text = "", bg = "white")
 lblclicks = Label(window, font = "Times 18", text = ("Клики:", clicksgame))
 mainbut = Button(window, width=70, height=20, bd = "3", bg = "#4869D6", activebackground="#FF8C00", text = "Начать тест", command=click)

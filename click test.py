45eca71b08bc62a6c84366cd9d5dd3fc7d0f93c4
n1ckxd <67688823+n1ckxd@users.noreply.github.com>
2020-07-24 17:28:17 +0300

    update
    добавлен режим подсчитывания кликов за 60 секунд

--- click test.py
+++ click test.py
@@ -15,12 +15,14 @@ colfsec = 0
 coltsec = 0
 coltrsec = 0
 colosec = 0
+colshssec = 0
 timemain = "tsec"
 def colosecplus():
     global timemain
     global coltsec
     global coltrsec
     global colfsec
+    global colshssec
     if colosec%2 != 0:
         osecbut.configure(bg = "gray")
         if coltsec%2 !=0:
@@ -32,6 +34,9 @@ def colosecplus():
         if coltrsec%2 !=0:
             coltrsec = 2
             trsecbut.configure(bg = "white")
+        if colshssec%2 !=0:
+            colshssec = 2
+            shssecbut.configure(bg = "white")
     else:
         osecbut.configure(bg = "white")
         timemain = "tsec"
@@ -40,6 +45,7 @@ def colfsecplus():
     global coltsec
     global coltrsec
     global colosec
+    global colshssec
     if colfsec%2 != 0:
         fsecbut.configure(bg = "gray")
         if coltsec%2 !=0:
@@ -51,6 +57,9 @@ def colfsecplus():
         if coltrsec%2 !=0:
             coltrsec = 2
             trsecbut.configure(bg = "white")
+        if colshssec%2 !=0:
+            colshssec = 2
+            shssecbut.configure(bg = "white")
     else:
         fsecbut.configure(bg = "white")
         timemain = "tsec"
@@ -59,6 +68,7 @@ def coltsecplus():
     global colfsec
     global coltrsec
     global colosec
+    global colshssec
     if coltsec%2 != 0:
         tsecbut.configure(bg = "gray")
         if colfsec%2 !=0:
@@ -70,6 +80,9 @@ def coltsecplus():
         if coltrsec%2 !=0:
             coltrsec = 2
             trsecbut.configure(bg = "white")
+        if colshssec%2 !=0:
+            colshssec = 2
+            shssecbut.configure(bg = "white")
     else:
         tsecbut.configure(bg = "white")
         timemain = "tsec"
@@ -78,6 +91,7 @@ def coltrsecplus():
     global colfsec
     global coltsec
     global colosec
+    global colshssec
     if coltrsec%2 != 0:
         trsecbut.configure(bg = "gray")
         if colfsec%2 !=0:
@@ -89,8 +103,34 @@ def coltrsecplus():
         if coltsec%2 !=0:
             coltsec = 2
             tsecbut.configure(bg = "white")
+        if colshssec%2 !=0:
+            colshssec = 2
+            shssecbut.configure(bg = "white")
     else:
         trsecbut.configure(bg = "white")
+        timemain = "tsec"
+def colshssecplus():
+    global timemain
+    global colfsec
+    global coltsec
+    global colosec
+    global coltrsec
+    if colshssec%2 != 0:
+        shssecbut.configure(bg = "gray")
+        if colfsec%2 !=0:
+            colfsec = 2
+            fsecbut.configure(bg = "white")
+        if colosec%2 !=0:
+            colosec = 2
+            osecbut.configure(bg = "white")
+        if coltsec%2 !=0:
+            coltsec = 2
+            tsecbut.configure(bg = "white")
+        if coltrsec%2 !=0:
+            coltrsec = 2
+            trsecbut.configure(bg = "white")
+    else:
+        shssecbut.configure(bg = "white")
         timemain = "tsec"
 def osec():
     global timemain
@@ -116,6 +156,12 @@ def trsec():
     timemain = "trsec"
     coltrsec = coltrsec + 1
     coltrsecplus()
+def shssec():
+    global timemain
+    global colshssec
+    timemain = "shssec"
+    colshssec = colshssec + 1
+    colshssecplus()
 def on_enter(e):
     mainbut['background'] = '#06266F'
 def on_leave(e):
@@ -137,6 +183,7 @@ def clear():
     fsecbut.pack()
     tsecbut.pack()
     trsecbut.pack()
+    shssecbut.pack()
 def timeplus10sec():
     global k
     if k == 1:
@@ -164,6 +211,13 @@ def timeplus30sec():
         k = 0
         if timegame < 1:
             Timer = threading.Timer(30, timeplusser30sec)
+            Timer.start()
+def timeplusshssec():
+    global k
+    if k == 1:
+        k = 0
+        if timegame < 1:
+            Timer = threading.Timer(60, timeplussershssec)
             Timer.start()
 def timeplusser10sec():
     global timegame
@@ -207,6 +261,20 @@ def timeplusserosec():
         messagebox.showinfo('Время вышло!', 'Сделано кликов: {}.\nВ среднем вы делаете {} кликов в секунду.'.format(clicksgame, crntclicks))
     timegame = 1
     clear()
+def timeplussershssec():
+    global timegame
+    global crntclicks
+    lblclicks['text'] = ("Клики: 0")
+    clicksgamenum = float(clicksgame)
+    crntclicks = (clicksgamenum / 60)
+    if str(crntclicks).endswith('.0'):
+        crntclicks = str(crntclicks)[:-2]
+    if str(crntclicks).endswith('1'):
+        messagebox.showinfo('Время вышло!', 'Сделано кликов: {}.\nВ среднем вы делаете {} клик в секунду.'.format(clicksgame, crntclicks))
+    else:
+        messagebox.showinfo('Время вышло!', 'Сделано кликов: {}.\nВ среднем вы делаете {} кликов в секунду.'.format(clicksgame, crntclicks))
+    timegame = 1
+    clear()
 def timeplusser30sec():
     global timegame
     global crntclicks
@@ -226,6 +294,8 @@ def click():
     fsecbut.pack_forget()
     trsecbut.pack_forget()
     osecbut.pack_forget()
+    shssecbut.pack_forget()
+    
     global k
     global clicksgame
     global timegame
@@ -253,7 +323,13 @@ def click():
         if timegame < 1:
             clicksgame = clicksgame+1
             lblclicks['text'] = ("Клики:", clicksgame)
-lbltitle = Label(window, font = "Times 13", text = "Приветствую вас в моей программе!\nЗдесь вы сможете узнать,сколько кликов вы сможете сделать за 10 или 5 секунд.\nПо умолчанию тест идёт на 10 секунд.\nДля того,чтобы увидеть все кнопки разверните окно.")
+    if timemain == "shssec":
+        timeplusshssec()
+        mainbut.configure(text = "Кликай!")
+        if timegame < 1:
+            clicksgame = clicksgame+1
+            lblclicks['text'] = ("Клики:", clicksgame)
+lbltitle = Label(window, font = "Times 13", text = "Приветствую вас в моей программе!\nЗдесь вы сможете узнать,сколько кликов вы сможете сделать за 1/5/10/30/60 секунд.\nПо умолчанию тест идёт на 10 секунд.\nДля того,чтобы увидеть все кнопки разверните окно.")
 lblspace = Label(window, text = "", bg = "white")
 lblclicks = Label(window, font = "Times 18", text = ("Клики:", clicksgame))
 mainbut = Button(window, width=70, height=20, bd = "3", bg = "#4869D6", activebackground="#FF8C00", text = "Начать тест", command=click)
@@ -261,6 +337,7 @@ osecbut = Button(window, width=10, height=3, bd = "3", bg = "white", activebackg
 fsecbut = Button(window, width=10, height=3, bd = "3", bg = "white", activebackground="gray", text = "5 секунд", command=fsec)
 tsecbut = Button(window, width=10, height=3, bd = "3", bg = "white", activebackground="gray", text = "10 секунд", command=tsec)
 trsecbut = Button(window, width=10, height=3, bd = "3", bg = "white", activebackground="gray", text = "30 секунд", command=trsec)
+shssecbut = Button(window, width=10, height=3, bd = "3", bg = "white", activebackground="gray", text = "60 секунд", command=shssec)
 mainbut.bind("<Enter>", on_enter)
 mainbut.bind("<Leave>", on_leave)
 lbltitle.pack()
@@ -271,5 +348,6 @@ osecbut.pack()
 fsecbut.pack()
 tsecbut.pack()
 trsecbut.pack()
+shssecbut.pack()
 window.mainloop()
 input()
